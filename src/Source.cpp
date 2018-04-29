c5be9cf92547df5e999d361671c1285ec8078329
Ruben Alvarez <ruben.amtz@outlook.com>
2018-04-29 18:58:31 +0100

    Working version 2.0
    Second update
    
    Co-Authored-By: Jesus Alvarez <jsgilberto@users.noreply.github.com>

--- src/Source.cpp
+++ src/Source.cpp
@@ -18,9 +18,9 @@ static bool paused;
 static bool selectObject = false;
 static bool startSelection = false;
 
+//the following const char* keys are up to date, they will be used from now on 29/04/2018
 static const char* keys =
-{ "{@tracker_algorithm |KCF | Tracker algorithm }"
-"{@video_name      |C:\\Users\\RubenAMtz\\Documents\\Bar_Path\\video.mp4| video name}"
+{ "{@tracker_algorithm |TrackerMIL | Tracker algorithm }"
 "{@video_path      |C:\\Users\\RubenAMtz\\Documents\\Bar_Path\\| }"
 "{@solution_path   |C:\\Users\\RubenAMtz\\Documents\\Bar_Path\\Procesados\\| Save folder}"
 "{@start_frame     |0| Start frame       }"
