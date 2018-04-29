05703f36f26477c187b8e623c445f401dc0ff124
Ruben Alvarez <ruben.amtz@outlook.com>
2018-04-29 21:27:34 +0100

    Working version 2.0
    Automatic circle detection, not yet implemented (erased)
    
    Co-Authored-By: Jesus Alvarez <jsgilberto@users.noreply.github.com>

--- src/Source.cpp
+++ src/Source.cpp
@@ -159,37 +159,6 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 
-	Mat prueba, src_gray;
-	/////////////////
-	cap >> prueba;
-	prueba.copyTo(prueba);
-	/// Convert it to gray
-	cvtColor(prueba, src_gray, CV_BGR2GRAY);
-
-	/// Reduce the noise so we avoid false circle detection
-	GaussianBlur(src_gray, src_gray, Size(9, 9), 2, 2);
-
-	vector<Vec3f> circles;
-
-	/// Apply the Hough Transform to find the circles
-	HoughCircles(src_gray, circles, CV_HOUGH_GRADIENT, 1, src_gray.rows / 8, 200, 100, 0, 500);
-
-	/// Draw the circles detected
-	for (size_t i = 0; i < circles.size(); i++)
-	{
-		Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-		int radius = cvRound(circles[i][2]);
-		// circle center
-		circle(prueba, center, 3, Scalar(0, 255, 0), -1, 8, 0);
-		// circle outline
-		circle(prueba, center, radius, Scalar(0, 0, 255), 3, 8, 0);
-	}
-
-	/// Show your results
-	namedWindow("Hough Circle Transform Demo", WINDOW_AUTOSIZE);
-	imshow("Hough Circle Transform Demo", prueba);
-	/////////////////
-
 	Mat frame;
 	paused = true;
 	//namedWindow("Tracking API", 1);
