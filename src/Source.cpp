f3a7e8fcb54481dae7655103fea39a104ceb3ae9
jsgilberto <alv.mtz94@gmail.com>
2018-04-30 23:09:44 +0200

    changed source to work with new structure

--- src/Source.cpp
+++ src/Source.cpp
@@ -1,23 +1,17 @@
-#include "opencv2/imgproc/imgproc.hpp"
-#include <opencv2/core/utility.hpp>
-#include <opencv2/tracking.hpp>
-#include <opencv2/tracking/tracking.hpp>
-#include <opencv2/tracking/tracker.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
-#include "opencv2/opencv.hpp"
+#include "Header.h"
+#include "globals.h"
 #include <iostream>
 #include <string>
 #include <cstring>
 
-using namespace std;
-using namespace cv;
 
-static Mat image;
-static Rect2d boundingBox;
-static bool paused;
-static bool selectObject = false;
-static bool startSelection = false;
+
+bool selectObject = false;
+bool paused = false;
+bool startSelection = false;
+Rect2d boundingBox;
+Mat image;
+bool initBoxWasGivenInCommandLine = false;
 
 //the following const char* keys are up to date, they will be used from now on 29/04/2018
 static const char* keys =
@@ -28,55 +22,6 @@ static const char* keys =
 "{@bounding_frame  |0,0,0,0| Initial bounding frame}" };
 
 
-static void onMouse(int event, int x, int y, int, void*)
-{
-	if (!selectObject)
-	{
-		switch (event)
-		{
-		case EVENT_LBUTTONDOWN:
-			//set origin of the bounding box
-			startSelection = true;
-			boundingBox.x = x;
-			boundingBox.y = y;
-			break;
-		case EVENT_LBUTTONUP:
-			//sei with and height of the bounding box
-			boundingBox.width = std::abs(x - boundingBox.x);
-			boundingBox.height = std::abs(y - boundingBox.y);
-			paused = false;
-			selectObject = true;
-			break;
-		case EVENT_MOUSEMOVE:
-
-			if (startSelection && !selectObject)
-			{
-				//draw the bounding box
-				Mat currentFrame;
-				image.copyTo(currentFrame);
-				rectangle(currentFrame, Point((int)boundingBox.x, (int)boundingBox.y), Point(x, y), Scalar(255, 0, 0), 2, 1);
-				imshow("Tracking API", currentFrame);
-			}
-			break;
-		}
-	}
-}
-
-static void help()
-{
-	cout << "\nThis example shows the functionality of \"Long-term optical tracking API\""
-		"-- pause video [p] and draw a bounding box around the target to start the tracker\n"
-		"Example of <video_name> is in opencv_extra/testdata/cv/tracking/\n"
-		"Call:\n"
-		"./tracker <tracker_algorithm> <video_name> <start_frame> [<bounding_frame>]\n"
-		"tracker_algorithm can be: MIL, BOOSTING, MEDIANFLOW, TLD\n"
-		<< endl;
-
-	cout << "\n\nHot keys: \n"
-		"\tq - quit the program\n"
-		"\tp - pause video\n";
-}
-
 int main(int argc, char** argv) {
 	CommandLineParser parser(argc, argv, keys);
 
@@ -85,7 +30,7 @@ int main(int argc, char** argv) {
 	//String tracker_algorithm = parser.get<String>(0);
 	//String video_name = parser.get<String>(1);
 	string video_name;
-	cout << "Name of video file: (include extension, i.e.: .mp4, .avi).\nThe root is in: "+ parser.get<String>(1) +"\n";
+	cout << "Name of video file: (include extension, i.e.: .mp4, .avi).\nThe root is in: " + parser.get<String>(1) + "\n";
 	getline(cin, video_name);
 	int start_frame = parser.get<int>(3);
 	string nombre;
@@ -96,7 +41,6 @@ int main(int argc, char** argv) {
 	int iLastY = -1;
 	Mat p;
 
-	//if (tracker_algorithm.empty() || video_name.empty())
 	if (video_name.empty())
 	{
 		help();
@@ -104,31 +48,8 @@ int main(int argc, char** argv) {
 	}
 
 	int coords[4] = { 0,0,0,0 };
-	bool initBoxWasGivenInCommandLine = false;
-	{
-		String initBoundingBox = parser.get<String>(4);
-		for (size_t npos = 0, pos = 0, ctr = 0; ctr<4; ctr++) {
-			npos = initBoundingBox.find_first_of(',', pos);
-			if (npos == string::npos && ctr<3) {
-				printf("bounding box should be given in format \"x1,y1,x2,y2\",where x's and y's are integer cordinates of opposed corners of bdd box\n");
-				printf("got: %s\n", initBoundingBox.substr(pos, string::npos).c_str());
-				printf("manual selection of bounding box will be employed\n");
-				break;
-			}
-			int num = atoi(initBoundingBox.substr(pos, (ctr == 3) ? (string::npos) : (npos - pos)).c_str());
-			if (num <= 0) {
-				printf("bounding box should be given in format \"x1,y1,x2,y2\",where x's and y's are integer cordinates of opposed corners of bdd box\n");
-				printf("got: %s\n", initBoundingBox.substr(pos, npos - pos).c_str());
-				printf("manual selection of bounding box will be employed\n");
-				break;
-			}
-			coords[ctr] = num;
-			pos = npos + 1;
-		}
-		if (coords[0]>0 && coords[1]>0 && coords[2]>0 && coords[3]>0) {
-			initBoxWasGivenInCommandLine = true;
-		}
-	}
+	String initBoundingBox = parser.get<String>(4);
+	getBoundingBox(initBoundingBox, coords);
 
 	//open the capture
 	VideoCapture cap;
@@ -137,12 +58,6 @@ int main(int argc, char** argv) {
 	double dheight = cap.get(CAP_PROP_FRAME_HEIGHT);
 	double dwidth = cap.get(CAP_PROP_FRAME_WIDTH);
 	Mat background = Mat::zeros(Size(dwidth, dheight), CV_8UC3);;
-	//imshow("Background", background);
-	//Mat logo = imread("C:\\Users\\hp\\Pictures\\toptier.png");
-	//Mat logo = imread("C:\\Users\\R.Alvarez\\Documents\\Camtasia Studio\\Bar Path\\toptier.png");
-	//resize(logo, logo, Size(logo.cols * 0.1, logo.rows * 0.1));
-	//logo.copyTo(background(Rect(background.cols-logo.cols, background.rows-logo.rows, logo.cols, logo.rows)));
-	//imshow("Back + logo", background);
 
 	//check later (29/04/2018)
 	//double fps = cap.get(CV_CAP_PROP_FPS);
@@ -161,13 +76,10 @@ int main(int argc, char** argv) {
 
 	Mat frame;
 	paused = true;
-	//namedWindow("Tracking API", 1);
 	namedWindow("Tracking API", WINDOW_NORMAL);
 	setMouseCallback("Tracking API", onMouse, 0);
 
 	//instantiates the specific Tracker
-	//Ptr<Tracker> tracker = Tracker::init(tracker_algorithm);
-
 	Ptr<Tracker> tracker = TrackerKCF::create();
 	if (tracker == NULL)
 	{
@@ -178,6 +90,7 @@ int main(int argc, char** argv) {
 	//Capture a temporary image from the camera
 	Mat imgTmp;
 	cap.read(imgTmp);
+	//resize(imgTmp, imgTmp, )
 
 	//Create a black image with the size as the camera output
 	Mat imgLines = Mat::zeros(imgTmp.size(), CV_8UC3);;
@@ -200,7 +113,6 @@ int main(int argc, char** argv) {
 	bool initialized = false;
 	int frameCounter = 0;
 
-	//VideoWriter oVideoWriter("C:\\Users\\hp\\Desktop\\Bar Path\\Procesados\\"+nombre+".avi", CV_FOURCC('M', 'P', '4', '2'), 30, Size(dwidth,dheight), true); //initialize the VideoWriter object 
 	VideoWriter oVideoWriter(parser.get<String>(2) + nombre + ".avi", cv::VideoWriter::fourcc('M', 'P', '4', '2'), 30, Size(dwidth, dheight), true); //initialize the VideoWriter object 
 
 	if (!oVideoWriter.isOpened()) //if not initialize the VideoWriter successfully, exit the program
@@ -239,24 +151,20 @@ int main(int argc, char** argv) {
 				if (tracker->update(frame, boundingBox))
 				{
 					rectangle(image, boundingBox, Scalar(255, 0, 0), 2, 1);
-					line(imgLines, Point(boundingBox.x + boundingBox.width / 2, boundingBox.y + boundingBox.height / 2), Point(iLastX, iLastY), Scalar(0, 255, 0), 2);
+					line(imgLines, 
+						Point(boundingBox.x + boundingBox.width / 2, boundingBox.y + boundingBox.height / 2), 
+						Point(iLastX, iLastY), Scalar(0, 255, 0), 2);
 				}
 			}
+
 			imshow("Tracking API", image + imgLines);
 			frameCounter++;
-
-			//fps = cap.get(CV_CAP_PROP_FPS);
-			//cout << "FPS: " << fps << endl;
-			//msec = cap.get(CV_CAP_PROP_POS_MSEC);
-			//cout << "Msec: " << fps << endl;
-
 			p = image + imgLines + background*0.4;
-
-			//water.copyTo(p(Rect(p.cols/2-water.cols/2, p.rows / 2 - water.rows / 2, water.cols, water.rows)));
 
 			oVideoWriter.write(p); //writer the frame into the file
 								   //imshow("MyVideo", p);
 		}
+
 		char c = (char)waitKey(2);
 		if (c == 'q')
 			break;
